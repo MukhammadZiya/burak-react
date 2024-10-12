7698b74061905bc8874a0d4f52e31a9459767988
MukhammadZiya <mukhammadziyaa@gmail.com>
2024-10-12 10:17:25 +0900

    feat: untill search  buttons

--- src/app/screens/homePage/productsPage/Products.tsx
+++ src/app/screens/homePage/productsPage/Products.tsx
@@ -1,3 +1,28 @@
+import React from "react";
+import { Container, Stack, TextField } from "@mui/material";
+
 export default function Products() {
-  return (<div>Products</div>)
+  return (
+    <div className="products">
+      <Container>
+        <Stack
+          className="products-main"
+          display="flex"
+          flexDirection="column"
+          alignItems="center"
+        >
+          <Stack className="product-wrapper" direction="row" spacing={2}>
+            <span>Burak Restaurant</span>
+            <div className="placeholder-input">Type here</div>
+          </Stack>
+
+          <Stack className="dishes-filter-section"></Stack>
+          <Stack className="list-category-section"></Stack>
+          <Stack className="pagination-section"></Stack>
+        </Stack>
+      </Container>
+      <div className="brands-logo"></div>
+      <div className="address"></div>
+    </div>
+  );
 }
